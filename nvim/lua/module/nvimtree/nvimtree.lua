cb2c078460c7213bc3d6756fbb975c9718e92740
root <root@debian.biandc.debian>
2022-11-29 16:07:33 +0800

    1

--- nvim/lua/module/nvimtree/nvimtree.lua
+++ nvim/lua/module/nvimtree/nvimtree.lua
@@ -2,5 +2,14 @@
 require'nvim-tree'.setup {
     git = {
         enable = false
+    },
+    view = {
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = { "<CR>", "o", "<2-LeftMouse>", "l" }, action = "edit" },
+            }
+        }
     }
+
 }
