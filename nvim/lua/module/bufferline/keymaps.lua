574697997b24fd8130bcdbc16026cb77cfb6d2cf
root <root@debian.biandc.debian>
2022-12-08 18:33:30 +0800

    1

--- nvim/lua/module/bufferline/keymaps.lua
+++ nvim/lua/module/bufferline/keymaps.lua
@@ -1,4 +1,8 @@
 
 local opts = { noremap=true, silent=true }
- vim.keymap.set('n', ';<tab>', ':BufferLineCycleNext<cr>', opts)
- vim.keymap.set('n', ';<s-tab>', ':BufferLineCyclePrev<cr>', opts)
+ vim.keymap.set('n', '<c-l>', ':BufferLineCycleNext<cr>', opts)
+ vim.keymap.set('n', '<c-h>', ':BufferLineCyclePrev<cr>', opts)
+ vim.keymap.set('n', ';dl', ':BufferLineCloseRight<cr>', opts)
+ vim.keymap.set('n', ';dh', ':BufferLineCloseLeft<cr>', opts)
+ vim.keymap.set('n', ';fl', ':BufferLineMoveNext<cr>', opts)
+ vim.keymap.set('n', ';fh', ':BufferLineMovePrev<cr>', opts)
