8cf8e001b72ca49908c51cb7d4a53791dc43d6d5
root <root@debian.biandc.debian>
2022-12-09 14:39:56 +0800

    1

--- nvim/lua/module/osc52/keymaps.lua
+++ nvim/lua/module/osc52/keymaps.lua
@@ -1,4 +1,4 @@
 
-local opts = { noremap=true }
-vim.keymap.set('n', ';y', require('osc52').copy_operator, opts)
-vim.keymap.set('v', ';y', require('osc52').copy_visual, opts)
+-- local opts = { noremap=true }
+-- vim.keymap.set('n', ';y', require('osc52').copy_operator, opts)
+-- vim.keymap.set('v', ';y', require('osc52').copy_visual, opts)
--- nvim/lua/module/osc52/osc52.lua
+++ nvim/lua/module/osc52/osc52.lua
@@ -4,3 +4,21 @@ require('osc52').setup {
   silent = false,  -- Disable message on successful copy
   trim = false,    -- Trim text before copy
 }
+
+local function copy(lines, _)
+    require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+    return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+vim.g.clipboard = {
+    name = 'osc52',
+    copy = {['+'] = copy, ['*'] = copy},
+    paste = {['+'] = paste, ['*'] = paste},
+}
+
+-- Now the '+' register will copy to system clipboard using OSC52
+-- vim.keymap.set('n', '<leader>c', '"+y')
+-- vim.keymap.set('n', '<leader>cc', '"+yy')
